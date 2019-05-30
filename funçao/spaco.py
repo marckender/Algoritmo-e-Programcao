f00dd58439f4d5d1b90a46eccd0e9226dfed8bb3
aluno <uffs@uffs.edu.br>
2019-05-30 14:26:28 -0300

    curso 30 maio

---
+++ funçao/spaco.py
@@ -0,0 +1,9 @@
+''' retorna um string sem espaço '''
+
+def teste(str):
+    st=''
+    for i in range(len(st)):
+        if not str[i]=="":
+            st+=str[i]
+    print(sr)        
+    return sr
