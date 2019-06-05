56033fa0484a419bcf38e5285f924b61afbc1d6f
Igor Bordin <igorbordinrs@gmail.com>
2019-06-05 13:42:26 -0300

    ...

---
+++ funçao/f_1.py
@@ -0,0 +1,7 @@
+def dobra(num):
+	num*=2
+	return num
+
+for  i in range (3):
+	num=int(input("Digite o valor: "))
+	print(dobra(num))
---
+++ funçao/f_2.py
@@ -0,0 +1,9 @@
+def dobra(num):
+	num*=2
+	return num
+
+
+def entra():
+	for  i in range (3):
+		num=int(input("Digite o valor: "))
+		print(dobra(num))
