da7acdf2c5fe92af9988597accba3399ba3283a5
aluno <uffs@uffs.edu.br>
2019-06-06 15:55:36 -0300

    ...

---
+++ matrix.py
@@ -0,0 +1,11 @@
+m=[[3,9,7,5],[10,5,15,20],[1,3,7,9],[6,7,8,9]]
+v=[]
+for linha in m:
+	soma=0
+
+	for coluna in linha:
+		soma=soma+linha[m]
+		print(linha)
+
+      #  print(coluna,end='')
+   # print('\n')
